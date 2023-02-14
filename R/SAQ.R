fa25b40bbdafa6bef5218dc1615675d1d0424711
Louis Rocconi <lrocconi@utk.edu>
2023-02-14 10:12:10 -0500

    saq data document

---
+++ R/SAQ.R
@@ -0,0 +1,25 @@
+#' SPSS Anxiety Questionnaire
+#'
+#' These data are from Andy Field's ... book name...
+#'
+#' **Note:** This dataset was imported from an SPSS .sav file using `haven` and therefore has variable attributes attached.
+#'
+#' @format A data frame with XXX rows and 23 variables:
+#' \describe{
+#'   \item{Question_01}{Statistics make me cry.}
+#'   \item{minority}{ethnicity status: other, minority}
+#'   \item{female}{gender status: female, male}
+#'   \item{ses}{socioeconomic status based on a standardized scale constructed from measures of parental occupation, education, and income}
+#'   \item{mathach}{a measure of math achievement}
+#'   \item{size}{school enrollment size}
+#'   \item{catholic}{school sector: public school or catholic school}
+#'   \item{pracad}{proportion of students in the academic track}
+#'   \item{disclim}{scale measuring disciplinary climate}
+#'   \item{himinty}{proportion of minority enrollment}
+#'   \item{meanses}{mean SES for each school}
+#' }
+#' @references{
+#'   \insertRef{raudenbush2002}{mlmhelpr}
+#' }
+#' @source \url{https://stats.oarc.ucla.edu/other/hlm/hlm-mlm/introduction-to-multilevel-modeling-using-hlm/}
+"hsb"
