5954a2577de430c3cbd9e866ed0e795208b662eb
Xavier Lynch <xavierlynch@Xaviers-MacBook-Air.local>
2023-02-21 09:36:36 -0500

    Updated SAQ file

--- R/SAQ.R
+++ R/SAQ.R
@@ -7,16 +7,28 @@
 #' @format A data frame with XXX rows and 23 variables:
 #' \describe{
 #'   \item{Question_01}{Statistics make me cry.}
-#'   \item{minority}{ethnicity status: other, minority}
-#'   \item{female}{gender status: female, male}
-#'   \item{ses}{socioeconomic status based on a standardized scale constructed from measures of parental occupation, education, and income}
-#'   \item{mathach}{a measure of math achievement}
-#'   \item{size}{school enrollment size}
-#'   \item{catholic}{school sector: public school or catholic school}
-#'   \item{pracad}{proportion of students in the academic track}
-#'   \item{disclim}{scale measuring disciplinary climate}
-#'   \item{himinty}{proportion of minority enrollment}
-#'   \item{meanses}{mean SES for each school}
+#'   \item{Question_02}{My friends will think I am stupid for not being able to cope with SPSS.}
+#'   \item{Question_03}{Standard deviations excite me.}
+#'   \item{Question_04}{I dream that Pearson is attacking me with correlation coefficients.}
+#'   \item{Question_05}{I don’t understand statistics.}
+#'   \item{Question_06}{I have little experience with computers.}
+#'   \item{Question_07}{All computers hate me.}
+#'   \item{Question_08}{I have never been good at mathematics.}
+#'   \item{Question_09}{My friends are better at statistics than me.}
+#'   \item{Question_10}{Computers are useful for only playing games.}
+#'   \item{Question_11}{I did badly at mathematics at school.}
+#'   \item{Question_12}{People try to tell you that SPSS makes statistics easier to understand but it doesn’t.}
+#'   \item{Question_13}{I worry that I will cause irreparable damage because of my incompetence with computers.}
+#'   \item{Question_14}{Computers have minds of their own and deliberately go wrong whenever I use them.}
+#'   \item{Question_15}{Computers are out to get me.}
+#'   \item{Question_16}{I weep openly at the mention of central tendency.}
+#'   \item{Question_17}{I slip into a coma whenever I see an equation.}
+#'   \item{Question_18}{SPSS always crashes when I try to use it.}
+#'   \item{Question_19}{Everybody looks at me when I use SPSS.}
+#'   \item{Question_20}{I can’t sleep for thoughts of eigenvectors.}
+#'   \item{Question_21}{I wake up under my duvet thinking that I am trapped under a normal distribution.}
+#'   \item{Question_22}{My friends are better at SPSS than I am.}
+#'   \item{Question_23}{If I am good at statistics people will think I am a nerd.}
 #' }
 #' @references{
 #'   \insertRef{raudenbush2002}{mlmhelpr}
