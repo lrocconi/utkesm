6f248deef0d095fd1a17796e25448330c4f7ac91
Louis Rocconi <lrocconi@utk.edu>
2023-04-11 10:03:22 -0400

    delete nsse files

---
+++ R/cleanMatrix.R
@@ -0,0 +1,24 @@
+#' Drop Matrix
+#'
+#' @param matrix Full matrix to be cleaned
+#' @param vars Variables to add/remove from matrix
+#'
+#' @return Returns a matrix with variables added or removed
+#' @export
+#'
+#' @description Drops/keeps variable names within the correlation matrix
+#'
+#'
+#' @examples
+#' data("iris")
+#' cor_matrix <- cor(iris[,[-5]])
+#' dropMatrix(cor_matrix, c("var1", "var2"))
+#'
+dropMatrix <- function(matrix, vars) {
+  remove <- vars
+  dropC <- colnames(matrix) %in% remove #removes from columns
+  dropR <- rownames(matrix) %in% remove #removes from rows
+
+  matrix[!dropR,!dropC]
+}
+"cleanMatrix"
