60375db8c40fe9b1413f08f4b0a7b566a1c7b86d
Louis Rocconi <lrocconi@utk.edu>
2024-11-20 12:16:53 -0500

    updated description for complexity

--- R/complexity.R
+++ R/complexity.R
@@ -2,7 +2,7 @@
 #'
 #' @param x Vector of factor loadings
 #'
-#' @return Returns the item complexity for each of the items
+#' @return Returns the item complexity for each of the items loading given
 #'
 #' @description This function compute Hoffman’s (1978) item complexity index. This is mainly for teaching and demonstration purposes since item complexity is returned by default in `psych::fa`. Item complexity is an indication of simple structure (i.e., items should load highly onto one factor and have weak loadings on others). Students often ask why the item complexity for particular item is greater than 2 when only one loading is displaying for that item. The issue arises when suppressing loadings less than a certain value. The weak loadings across multiple factors is usually what is causing the complexity score to be greater than 2. To help students better understand item complexity, I wrote this function. Simply, supply the function a vector of factor loadings and it will compute item complexity.
 #'
